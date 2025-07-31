a210054fbd0240995c0777bf0c5264f776aaab44
astrealRBLX <impulsiv.12@gmail.com>
2025-07-31 17:21:47 -0400

    Update hotkey utils

--- src/utils/hotkeyUtils.ts
+++ src/utils/hotkeyUtils.ts
@@ -1,21 +1,52 @@
-import { peek } from '@rbxts/charm';
+import { peek, subscribe } from '@rbxts/charm';
+import { useEffect, useRef } from '@rbxts/react';
+import { RunService } from '@rbxts/services';
 import { pressedKeys } from 'state/editor';
 
 export enum HotkeyIDs {
+  // Scrubber contextual
   ScrubberSnapTimestamp,
   ScrubberSnapKeyframe,
+
+  // Scrubber static
+  ScrubberPreview,
+  ScrubberNudgeLeft,
+  ScrubberNudgeLeftSlow,
+  ScrubberNudgeLeftFast,
+  ScrubberNudgeRight,
+  ScrubberNudgeRightSlow,
+  ScrubberNudgeRightFast,
+
+  // Keyframes contextual
   KeyframesSelectMultiple,
+  KeyframesSelectRange,
   KeyframesDragDeselect,
   KeyframesDragInvertSelection,
+
+  // Keyframes static
+  KeyframesInsert,
+  KeyframesDeleteSelected,
 }
 
-//
 const hotkeys = new Map<HotkeyIDs, Enum.KeyCode[]>([
   [HotkeyIDs.ScrubberSnapTimestamp, [Enum.KeyCode.LeftControl]],
-  [HotkeyIDs.ScrubberSnapKeyframe, [Enum.KeyCode.LeftControl, Enum.KeyCode.LeftShift]],
+  [HotkeyIDs.ScrubberSnapKeyframe, [Enum.KeyCode.LeftShift]],
+
+  [HotkeyIDs.ScrubberPreview, [Enum.KeyCode.Space]],
+  [HotkeyIDs.ScrubberNudgeLeft, [Enum.KeyCode.Q]],
+  [HotkeyIDs.ScrubberNudgeLeftSlow, [Enum.KeyCode.Q, Enum.KeyCode.LeftControl]],
+  [HotkeyIDs.ScrubberNudgeLeftFast, [Enum.KeyCode.Q, Enum.KeyCode.LeftShift]],
+  [HotkeyIDs.ScrubberNudgeRight, [Enum.KeyCode.E]],
+  [HotkeyIDs.ScrubberNudgeRightSlow, [Enum.KeyCode.E, Enum.KeyCode.LeftControl]],
+  [HotkeyIDs.ScrubberNudgeRightFast, [Enum.KeyCode.E, Enum.KeyCode.LeftShift]],
+
   [HotkeyIDs.KeyframesSelectMultiple, [Enum.KeyCode.LeftControl]],
+  [HotkeyIDs.KeyframesSelectRange, [Enum.KeyCode.LeftShift]],
   [HotkeyIDs.KeyframesDragDeselect, [Enum.KeyCode.LeftControl]],
   [HotkeyIDs.KeyframesDragInvertSelection, [Enum.KeyCode.LeftControl, Enum.KeyCode.LeftShift]],
+
+  [HotkeyIDs.KeyframesInsert, [Enum.KeyCode.I]],
+  [HotkeyIDs.KeyframesDeleteSelected, [Enum.KeyCode.Delete]],
 ]);
 
 export function isHotkeyPressed(id: HotkeyIDs) {
@@ -35,4 +66,55 @@ export function isHotkeyPressed(id: HotkeyIDs) {
   if (matchedKeys !== keys.size()) return false;
 
   return true;
+}
+
+export function useHotkey(
+  id: HotkeyIDs,
+  withContextHotkeys: HotkeyIDs[],
+  callback: (contextHotkey?: HotkeyIDs) => void,
+  dependencies: React.DependencyList
+) {
+  useEffect(() => {
+    const cleanup = subscribe(pressedKeys, () => {
+      if (isHotkeyPressed(id)) {
+        callback();
+      } else if (withContextHotkeys.size() > 0) {
+        withContextHotkeys.forEach((ctxKey) => {
+          if (isHotkeyPressed(ctxKey)) callback(ctxKey);
+        });
+      }
+    });
+
+    return cleanup;
+  }, dependencies);
+}
+
+export function useHotkeyDown(
+  id: HotkeyIDs,
+  withContextHotkeys: HotkeyIDs[],
+  callbackDelay: number,
+  callback: (contextHotkey?: HotkeyIDs) => void,
+  dependencies: React.DependencyList
+) {
+  const toNextCall = useRef(0);
+
+  useEffect(() => {
+    const conn = RunService.RenderStepped.Connect(() => {
+      if (tick() >= toNextCall.current) {
+        if (isHotkeyPressed(id)) {
+          callback();
+        } else if (withContextHotkeys.size() > 0) {
+          withContextHotkeys.forEach((ctxKey) => {
+            if (isHotkeyPressed(ctxKey)) callback(ctxKey);
+          });
+        }
+
+        toNextCall.current = tick() + callbackDelay;
+      }
+    });
+
+    return () => {
+      conn.Disconnect();
+    };
+  }, dependencies);
 }
